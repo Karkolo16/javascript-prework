ecb48d0d3b1799ae4c28ed6e29ad06003a4d3a7a
Karolina <ka.kolomyjec@gmail.com>
2022-07-23 11:40:13 +0200

    Add playerMove else/if/elseIf to script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ if (randomNumber == '3') {
   computerMove = 'nożyce';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+INPUT
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'Błędny wybór';
+}
+printMessage('Twój ruch: ' + playerMove);
