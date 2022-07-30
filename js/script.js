9c42d3236f8d9ef7f940d03cd40f2ac18647fbb3
Karolina <ka.kolomyjec@gmail.com>
2022-07-30 11:10:47 +0200

    add const to computerMove

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const buttonScissors = document.getElementById('button-scissors');
 function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
-  var argMoveId, argPlayerMove, argComputerMove, computerMove,playerInput;
+  var argMoveId, argPlayerMove, argComputerMove, playerInput;
 
 
 function getMoveName(argMoveId) {
@@ -44,7 +44,7 @@ const playerMove = argButtonName;
 console.log('ruch gracza to: ' + playerMove);
 const randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
+const computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
   
